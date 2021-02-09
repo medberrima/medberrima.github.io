35f93957f9798de6544da62748c63c5e300abcb9
Med <medberrima0@gmail.com>
2021-02-09 17:53:26 +0100

    master

--- form.php
+++ form.php
@@ -1,16 +1,19 @@
 <?php
-  $name= $_POST['Name'] ;
-  $mail= $_POST['Mail'] ;
-  $subject= $_POST['Subject'] ;
-  $message= $_POST['Message'] ;
-  
-  $to ="medberrima0@gmail.com";
-  $body ="";
-  
-  $body .="form  : ".$name ."\r\n";
-  $body .="Email :  ".$mail ."\r\n";
-  $body .="Message :  ".$message ."\r\n";
-  
-  mail($to,$subject,$body);
+  //get data from form  
+  $name = $_POST['name'];
+  $email= $_POST['email'];
+  $message= $_POST['message'];
+  $subject= $_POST['subject'];
 
+  $to = "medberrima0@gmail.com";
+
+  $txt ="mail from website Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+
+  $headers = "From: noreply@yoursite.com" . "\r\n" .
+  "CC: somebodyelse@example.com";
+  if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+  }
+//redirect
+header("Location:thankyou.html");
 ?>
